3c16c0de49e5875631a00c9f9941ef272df74232
JoelJosephJecob <joeljosephjecob@gmail.com>
2024-09-24 15:24:54 +0530

    Add files via upload

--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/4VP23AI019/EXP4/FULLSUBS_isim_beh.exe" -prj "D:/4VP23AI019/EXP4/FULLSUBS_beh.prj" "work.FULLSUBS" "work.glbl" 
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/4VP23AI019/EXP7/demux18_isim_beh.exe" -prj "D:/4VP23AI019/EXP7/demux18_beh.prj" "work.demux18" "work.glbl" 
